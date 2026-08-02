00b156d2b8f7ce03bd4d760bb3bc0fe9761a6a13
janice632-web <janice632@gmail.com>
2026-08-02 22:53:17 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE="osaka2026-v1";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./assets/icons/icon-192.png","./assets/icons/icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));});
